c0b3044d8c1fe3252db59a1e523e586b13a0ad89
Terry Upton <terry@amasci.co.uk>
2014-05-07 17:32:50 +0100

    Removed save the orphans file as not used and causes issues. Might add again in future if necessary.

--- index.php
+++ index.php
@@ -236,7 +236,6 @@
 <!--<script src="//ajax.googleapis.com/ajax/libs/jquery/1.10.2/jquery.min.js"></script>
 <script>window.jQuery || document.write('<script src="/assets/js/jquery.min.js"><\/script>')</script>-->
 
-<script type="text/javascript" src="/assets/js/savetheorphans.min.js"></script>
 <script type="text/javascript" src="/assets/js/functions.min.js"></script>
 
 <!--[if IE 7]>
