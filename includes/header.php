f9335c87a8c5c932cf17da4e3b194094f02cd0de
Terry Upton <terry@amasci.co.uk>
2015-04-20 09:33:15 +0100

    Added PHP for use to call header function

--- includes/header.php
+++ includes/header.php
@@ -18,3 +18,42 @@ function do_html_header (
 HEADER CONTENT HERE
 
 <?php } ?>
+
+
+<?php
+
+//This goes into the page.
+
+// This is optional based on the website requirements
+$pageclass = 'about';
+
+include($_SERVER['DOCUMENT_ROOT'].'/includes/header.php');
+include($_SERVER['DOCUMENT_ROOT'].'/includes/footer.php');
+
+do_html_header (
+	//$pageNumber (unique page number - for menu highlighting)
+	'',
+
+	//$subpageNumber (child pages - for sub menu highlighting)
+	'',
+
+	//$section (name of the section for menu highlighting)
+	'about',
+
+	//$refText (unique ref text to reference the page or group of pages)
+	'',
+
+	//$metaTitle
+	'About Cladwinds Limited',
+
+	//$metaKeywords
+	'Meta Keywords',
+
+	//$metaDescription
+	'Meta Description',
+
+	//$template (used to reference different template layouts)
+	$pageclass
+);
+
+?>
